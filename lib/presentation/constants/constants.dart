ec3ce44b8f81a7ccc4372de0e493624707136cd9
Utitofon-Udoekong <mosesrahul37@gmail.com>
2023-11-13 21:46:49 +0100

    Remove unnecessary constants

--- lib/presentation/constants/constants.dart
+++ lib/presentation/constants/constants.dart
@@ -1,24 +1,9 @@
-///Hive box name for authentication
-
-class AppStrings{}
-const encAuthBox = 'auth_box_enc';
-const encPassword = 'auth_box_enc_password';
-const encUserName = 'auth_box_enc_username';
+///Hive box name for Forms
 const encAuthBoxKey = 'auth_box_enc_key';
-const farmInventoryBox = 'farm_inventory_box';
-const userModelBox = 'user_model_box';
+const encAgroFromBox = 'agro_form_box_enc_key';
+const encHealthFormBox = 'health_form_box_enc_key';
+const encTransportFormBox = 'transport_form_box_enc_key';
 
-const userSurname = 'user_model_box_surname';
-const userOtherNames = 'user_model_box_otherNames';
-const userAge = 'user_model_box_age';
-const userGender = 'user_model_box_gender';
-const userMaritalStatus = 'user_model_box_maritalStatus';
-const userNoOfChildren = 'user_model_box_noOfChildren';
-const userNoOfSpouses = 'user_model_box_noOfSpouses';
-const userNextOfKin = 'user_model_box_nextOfKin';
-const userContactNumber = 'user_model_box_contactNumber';
-const userAltContactNumber = 'user_model_box_altContactNumber';
-const userPhoneNumberOfNextOfKin = 'user_model_box_phoneNumberOfNextOfKin';
 /// Image constant class
 class Images{
   /// app logo address
